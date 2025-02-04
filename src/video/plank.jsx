09ca4f68e6756931e8019869643c9867ab5ef0f6
eslam2semedo <eslamsemedo@gmail.com>
2025-02-05 00:56:54 +0200

    .

---
+++ src/video/plank.jsx
@@ -0,0 +1,16 @@
+
+
+import React, { useRef, useEffect, useState } from 'react';
+// import { Pose, POSE_CONNECTIONS, POSE_LANDMARKS } from '@mediapipe/pose';
+// import { Camera } from '@mediapipe/camera_utils';
+// import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
+
+function plank() {
+
+
+  return (
+    <><h1>eslam</h1></>
+  );
+}
+
+export default plank;
